d1f368112d47c853c83275435cb19f6beca528e8
Austin Zuo <azuo@Austins-MacBook-Pro.local>
2020-06-05 11:07:07 -0700

    Spring Initalizr

---
+++ creditcardgenerator/src/main/java/CCG/creditcardgenerator/CreditcardgeneratorApplication.java
@@ -0,0 +1,13 @@
+package CCG.creditcardgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardgeneratorApplication.class, args);
+	}
+
+}
---
+++ creditcardgenerator/src/test/java/CCG/creditcardgenerator/CreditcardgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package CCG.creditcardgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
